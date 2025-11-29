a80598eee1c573970f349976a43454fd2f3f72d4
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-11-29 18:53:11 +0700

    fix_bugs

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import express from 'express';
-import { PORT } from './utils/env-utils';
-import { publicRouter } from './routes/public_api';
-import { errorMiddleware } from './middlewares/errorMiddleware';
+import { PORT } from './utils/env-util';
+import { publicRouter } from './routes/public-api';
+import { errorMiddleware } from './middlewares/error-middleware';
 import { privateRouter } from './routes/private-api';
 
 const app = express();
 
 app.use(express.json());
-app.use("/api/", publicRouter);
-app.use("/api/", privateRouter);
+app.use("/api", publicRouter);
+app.use("/api", privateRouter);
 app.use(errorMiddleware);
-app.listen(PORT || 3000, () => {
-  console.log(`connected to port ${PORT}`);
-});
 
+const port = Number(PORT) || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    console.log(`url: http://localhost:${port}`)
+})
