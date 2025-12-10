ae41605413e539c1e018dc5ec12ea5f5f785fe88
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-12-10 19:28:31 +0700

    add_seeding_not_done

--- src/models/learning-model.ts
+++ src/models/learning-model.ts
@@ -1,9 +1,42 @@
 import { string } from "zod"
+import { Learning } from "@prisma/client";
 
 export interface LearningResponse{
-    id: string;
+    id: number;
+    title: string;
+    description: string;
+    video_url: string;
+    createdAt: Date;
+    updatedAt: Date;
 }
 
-export interface AllLearningResponse{
+export interface LearningProgressResponse{
+    id: number;
+    user_id: number;
+    learning_id: number;
+    isCompleted: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+}
 
+export function toLearningProgressResponse(learningProgress: LearningProgressResponse): LearningProgressResponse{
+    return {
+        id: learningProgress.id,
+        user_id: learningProgress.user_id,
+        learning_id: learningProgress.learning_id,
+        isCompleted: learningProgress.isCompleted,
+        createdAt: learningProgress.createdAt,
+        updatedAt: learningProgress.updatedAt,
+    }
+}
+
+export function toLearningResponse(Learning: Learning): LearningResponse{
+    return {
+        id: Learning.id,
+        title: Learning.title,
+        description: Learning.description,
+        video_url: Learning.video_url,
+        createdAt: Learning.createdAt,
+        updatedAt: Learning.updatedAt,
+    }
 }
